e7b0d0069753f583c0dd817dd690b5b2237a09ab
Sergio <sergio.mahiarello@gmail.com>
2024-11-26 21:22:56 +0100

    Navbar title

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -3,15 +3,26 @@
 import Sidebar from '../../components/Dashboard/Sidebar/Sidebar';
 import Navbar from '../../components/Dashboard/Navbar';
 
+import { useState } from 'react';
+
 const DashboardLayout = ({ children }) => {
+
+    const [navBarTitle, setNavbarTitle] = useState("");
+    const [navBarDesc, setNavbarDesc] = useState("");
+
+    const handleNavbarChange = (title, desc) => {
+        setNavbarTitle(title);
+        setNavbarDesc(desc);
+    };
+
     return (
         <div className="flex h-screen bg-gray-100 overflow-hidden">
             <div className="w-1/5">
-                <Sidebar />
+                <Sidebar navBarChangeHandler={handleNavbarChange} />
             </div>
 
             <div className="w-4/5 flex flex-col">
-                <Navbar sectionName="Test" sectionDesc="This is a test section"/>
+                <Navbar sectionName={navBarTitle} sectionDesc={navBarDesc} />
                 <main className="flex-1 overflow-auto p-3 bg-slate-200">{children}</main>
             </div>
         </div>
--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@
 import SidebarItem from "./SidebarItem";
 import Image from 'next/image';
 
-export default function Sidebar() {
+export default function Sidebar({ navBarChangeHandler }) {
     return (
         <div className="h-screen bg-gray-100 p-4 border border-red-600">
             <div className="flex justify-center mb-6 border border-red-600 ">
@@ -12,9 +12,24 @@ export default function Sidebar() {
             <div className="border border-red-600">
                 <p className="text-gray-900">MENÚ</p>
                 <div className="mt-2">
-                    <SidebarItem title="Clientes" icon="/icons/clients.svg" href="/dashboard/clients" />
-                    <SidebarItem title="Proyectos" icon="/icons/projects.svg" href="/dashboard/projects" />
-                    <SidebarItem title="Albaranes" icon="/icons/albaranes.svg" href="/dashboard/albaranes" />
+                    <SidebarItem
+                        title="Clientes"
+                        icon="/icons/clients.svg"
+                        href="/dashboard/clients"
+                        onClick={() => navBarChangeHandler("Clientes", "Gestión de clientes")}
+                    />
+                    <SidebarItem
+                        title="Proyectos"
+                        icon="/icons/projects.svg"
+                        href="/dashboard/projects"
+                        onClick={() => navBarChangeHandler("Proyectos", "Gestión de proyectos")}
+                    />
+                    <SidebarItem
+                        title="Albaranes"
+                        icon="/icons/albaranes.svg"
+                        href="/dashboard/albaranes"
+                        onClick={() => navBarChangeHandler("Albaranes", "Gestión de albaranes")}
+                    />
                 </div>
             </div>
         </div>
