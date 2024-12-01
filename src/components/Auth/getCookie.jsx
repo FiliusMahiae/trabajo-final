6cc93cab26e5843b66237a09f6400823a2016126
Sergio <sergio.mahiarello@gmail.com>
2024-12-01 12:27:07 +0100

    Create getCookie.jsx

---
+++ src/components/Auth/getCookie.jsx
@@ -0,0 +1,12 @@
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Verifica si la cookie empieza con el nombre especificado
+      if (cookie.startsWith(`${name}=`)) {
+        return cookie.substring(name.length + 1); // Devuelve el valor de la cookie
+      }
+    }
+    return null; // Si no encuentra la cookie
+  }
+  
