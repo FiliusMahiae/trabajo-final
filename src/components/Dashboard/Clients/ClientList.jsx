a20058b3cab6368f544742fa2ac0261c1af0297a
Sergio <sergio.mahiarello@gmail.com>
2024-12-07 11:16:31 +0100

    Update ClientList.jsx

--- src/components/Dashboard/Clients/ClientList.jsx
+++ src/components/Dashboard/Clients/ClientList.jsx
@@ -2,10 +2,12 @@ import ClientCard from "./ClientCard";
 
 export default function ClientsList({ clients, onSelect }) {
   return (
-    <ul className="space-y-4">
-      {clients.map((client) => (
-        <ClientCard key={client._id} client={client} onSelect={onSelect} />
-      ))}
-    </ul>
+    <div className="h-[calc(100vh-200px)] overflow-y-auto border border-gray-300 rounded-lg shadow-md">
+      <ul className="space-y-4 p-4">
+        {clients.map((client) => (
+          <ClientCard key={client._id} client={client} onSelect={onSelect} />
+        ))}
+      </ul>
+    </div>
   );
 }
